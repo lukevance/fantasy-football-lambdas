38b537346a7c14264c206b1658cab5ce305efd1b
Luke Vance <luke@cloud-elements.com>
2018-09-29 11:31:34 -0600

    updaed path params

--- season-summary/index.js
+++ season-summary/index.js
@@ -3,7 +3,7 @@ const seasonSummary = require('./seasonSummary');
 exports.handler = async (event) => {
     // call season summary, check for errors from API
     if (event.pathParameters && event.pathParameters.hasOwnProperty('leagueId')){
-        const data = await seasonSummary(event.pathParameters.proxy).catch(err => err);
+        const data = await seasonSummary(event.pathParameters.leagueId).catch(err => err);
         if (data.games){
             const response = {
                 statusCode: 200,
