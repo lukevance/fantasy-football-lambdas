c211767cf52a184c867e6536418e108df6b2cfe6
Luke Vance <luke@cloud-elements.com>
2018-10-01 13:50:13 -0600

    added headers for cors issue and improved response object construction

--- season-summary/index.js
+++ season-summary/index.js
@@ -1,26 +1,30 @@
 const seasonSummary = require('./seasonSummary');
 
 exports.handler = async (event) => {
+    // establish default response properties
+    const response = {
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    };
     // call season summary, check for errors from API
-    if (event.pathParameters && event.pathParameters.hasOwnProperty('leagueId')){
+    if (event.pathParameters && event.pathParameters.hasOwnProperty('leagueId')) {
         const data = await seasonSummary(event.pathParameters.leagueId).catch(err => err);
-        if (data.gamesCompleted && data.teams){
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify(data)
-            };
-            return response;
+        // if useful data came back add it to the response body
+        if (data.gamesCompleted && data.teams) {
+            response.statusCode = 200;
+            response.body = JSON.stringify(data);
         } else {
-            const response = {
-                statusCode: 401,
-                body: JSON.stringify(data)
-            };
-            return response;
+            // if no useful data, assume private league and pass error
+            response.statusCode = 401;
+            response.body = data.message;
         }
     } else {
-        return {
-            statusCode: 404,
-            body: 'no {leagueId} provided '
-        }
+        // if no leagueId was passed through ... should be handled by API gateway
+        response.statusCode = 404;
+        response.body = {
+            message: 'no {leagueId} provided'
+        };
     }
+    return response;
 };
